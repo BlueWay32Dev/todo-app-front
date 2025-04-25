7f77d21d815026fa75dca659fd665682f8ee861b
Nicolas roa <bluesys64@Nicolass-MacBook-Pro.local>
2025-04-26 01:26:42 +1000

    Se configuran los entornos desarrollo y produccion

--- src/enviroments/environment.ts
+++ src/enviroments/environment.ts
@@ -1,11 +1,6 @@
 /// <reference types="vite/client" />
 
 export const environment = {
-<<<<<<< HEAD
-  production: true,
-  apiUrl: import.meta.env.NG_APP_API_URL
-=======
   production: false,
   apiUrl: 'http://localhost:5001/todo-app-backend-nicolas-roa/us-central1/api'
->>>>>>> 6cccf4e (Se configuran los entornos desarrollo y produccion)
 };
