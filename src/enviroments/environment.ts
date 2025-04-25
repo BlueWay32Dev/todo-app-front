271c9730c8fc04d0e02370a5fab1f6c40280a00f
Nicolas roa <bluesys64@Nicolass-MacBook-Pro.local>
2025-04-26 00:48:02 +1000

    Se realizan modificaciones en angular.json y creacion de enviroment.prod.ts

--- src/enviroments/environment.ts
+++ src/enviroments/environment.ts
@@ -1,4 +1,6 @@
+/// <reference types="vite/client" />
+
 export const environment = {
-  production: true,
-  apiUrl: import.meta.env['NG_APP_API_URL'] || 'http://localhost:5001/todo-app-backend-nicolas-roa/us-central1/api'
+  production: false,
+  apiUrl: 'http://localhost:5001/todo-app-backend-nicolas-roa/us-central1/api'
 };
